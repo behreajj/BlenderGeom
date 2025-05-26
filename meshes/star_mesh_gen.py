ef2c6da7d9d43485fb3735851de45e5371e38bda
Jeremy Behreandt <behreandtjeremy@gmail.com>
2025-05-25 23:47:19 -0500

    Star mesh Remove unused methods.

--- meshes/star_mesh_gen.py
+++ meshes/star_mesh_gen.py
@@ -120,20 +120,6 @@ class StarMeshMaker(bpy.types.Operator):
                 bm_face_loop.vert.normal = vns[vn_loop[k]]
 
         return bm
-
-    @staticmethod
-    def rotate_z(v, cosa, sina):
-        return (cosa * v[0] - sina * v[1],
-                cosa * v[1] + sina * v[0],
-                0.0)
-
-    @staticmethod
-    def scale(v, s):
-        return (v[0] * s, v[1] * s, 0.0)
-
-    @staticmethod
-    def translate(v, t):
-        return (v[0] + t[0], v[1] + t[1], 0.0)
     
     def execute(self, context):
         sectors = self.sectors
