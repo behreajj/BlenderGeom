1d251abdb163cb0f6712619f5b20f161461480b6
Jeremy Behreandt <behreandtjeremy@gmail.com>
2025-05-29 20:57:25 -0500

    Remove ogee curve gen.

--- curves/ogee_curve_gen.py
+++
@@ -1,9 +0,0 @@
-# https://en.wikipedia.org/wiki/Ogee
-# Three centered ogee:
-# https://www.youtube.com/watch?v=MxDSACbnEAk
-# Four centered ogee:
-# https://www.youtube.com/watch?v=cY-s3smT2PY
-# Tudor arch:
-# https://www.youtube.com/watch?v=T5PgU2V8YC4
-
-# Trefoil, Quatrefoil, Cinquefoil
