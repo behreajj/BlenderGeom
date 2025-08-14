60722c5ca94a8786e6f111456e37b8d84a76deb9
Jeremy Behreandt <behreandtjeremy@gmail.com>
2025-08-13 22:37:24 -0500

    Tudor arch mesh Set default extrusion offset to 1.0.

--- meshes/tudor_arch_mesh_gen.py
+++ meshes/tudor_arch_mesh_gen.py
@@ -55,7 +55,7 @@ class TudorArchMeshMaker(bpy.types.Operator):
     arch_offset: FloatProperty(
         name="Offset",
         description="Arch weight offset",
-        default=0.0,
+        default=1.0,
         step=1,
         precision=3,
         min=-1.0,
